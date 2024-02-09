080000a05f7081e7e9e06ae55ce5f7d828abb408
Clayton Zickerman <cczickerman@gmail.com>
2024-02-09 17:31:20 -0500

    inital

---
+++ README.md
@@ -0,0 +1,6 @@
+# Vite Template
+> Simplistic vite template using typescript, tailwind, and shadcn/ui 
+
+This website was mainly built as a utility for myself to use in order to expedite my own development process.
+
+You are free to use this code however you like but do not expect any form of support/maintenance on this repo.
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="flex flex-col space-y-8 justify-center items-center">
+      <h1 className="mt-14 text-5xl">
+        <span className="text-fuchsia-600">Vite</span> Template
+      </h1>
+      <h2>
+        This project uses{" "}
+        <a href="https://ui.shadcn.com/" className="text-blue-500 hover:text-blue-400 transition-colors">
+          shadcn/ui
+        </a>
+        , refer to their website for usage
+      </h2>
+    </div>
+  );
+}
+
+export default App;
